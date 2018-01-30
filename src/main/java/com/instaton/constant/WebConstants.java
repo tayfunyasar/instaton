283c69c4e9f44360f647d0af00c7d9d1c269a432
Tayfun Yaşar <tayfun.yasar@eteration.com>
2018-01-30 22:41:33 +0300

    run project wo profile

--- src/main/java/com/instaton/constant/WebConstants.java
+++ src/main/java/com/instaton/constant/WebConstants.java
@@ -1,29 +1,21 @@
 package com.instaton.constant;
 
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
+import java.util.List;
 
-@PropertySource(value = { "classpath:instaton-${instaton.profile}.properties" }, ignoreResourceNotFound = true)
-@Lazy(false)
-@Component
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class WebConstants {
-
-	public static final int AWAIT_TERMINATION_SECONDS = 500;
 
 	public static final String PAGE_ERROR_ACCESSDENIED = "/error/access-denied";
 	public static final String PAGE_ERROR_UNAUTHORIZED = "/error/un-authorized";
-	public static final String PAGE_HOME = "/anasayfa";
-	public static final String PAGE_ROOT = "/";
-	public static final String PAGE_SERVICE_NOT_AVAILABLE = "/error/service-not-available";
-	public static final String SSO_LOGOUT_URL = "/api/logout";
-	public static final String SSO_SESSION_HEARTBEAT = "/api/login/heartbeat";
 
-	public String PARTNER_GATEWAY_screenCode;// screenCode
+	@Value("#{'${instaton.twitter.searchkeywords}'.split(',')}")
+	private List<String> keywordList;
 
-	// @Value("${partner.gateway.screenCode}")
-	public void setPGatewayScreenCode(String screenCode) {
-		this.PARTNER_GATEWAY_screenCode = screenCode;
+	public List<String> getKeywordList() {
+		return this.keywordList;
 	}
 
 }
